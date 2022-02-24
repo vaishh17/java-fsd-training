b00c63d74750991645bf67a6b94a5d4555200edc
vaishh17 <99461616+vaishh17@users.noreply.github.com>
2022-02-24 17:21:18 +0530

    Add files via upload

---
+++ MySQL/Sql.sql
@@ -0,0 +1,4 @@
+create database emp;
+use emp;
+create table user905(id int not null ,name char (50),password varchar(50),email varchar(50), country char(50), primary key(id));
+select *from user905;
