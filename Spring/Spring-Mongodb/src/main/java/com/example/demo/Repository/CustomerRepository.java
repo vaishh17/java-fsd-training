a425ede71c25a11280ebff1350db02dcb4e578b6
vaishh17 <99461616+vaishh17@users.noreply.github.com>
2022-03-29 09:46:36 +0530

    Delete Spring/Spring-Mongodb directory

--- Spring/Spring-Mongodb/src/main/java/com/example/demo/Repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.model.Customer;
-
-public interface CustomerRepository extends MongoRepository<Customer,Integer> {
-
-}
